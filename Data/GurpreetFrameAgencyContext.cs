8743331f14f2e5d4a4b10dc2965a7710834bb867
W0824816 <W0824816@myscc.ca>
2023-10-11 19:36:06 -0400

    commit

---
+++ Data/GurpreetFrameAgencyContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GurpreetFrameAgency.Models;
+
+namespace GurpreetFrameAgency.Data
+{
+    public class GurpreetFrameAgencyContext : DbContext
+    {
+        public GurpreetFrameAgencyContext(DbContextOptions<GurpreetFrameAgencyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ Models/Frame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GurpreetFrameAgency.Models
+{
+    public class Frame
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ManufacturingDate { get; set; }
+        public string Color { get; set; }
+        public int Rating { get; set; }
+        public decimal Price { get; set; }
+    }
+}
