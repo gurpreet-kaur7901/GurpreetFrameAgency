64c311c9f4ac39ba433725380930a5ac7f6328e3
W0824816 <W0824816@myscc.ca>
2023-10-12 21:44:23 -0400

    ADD ABOUT US

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace GurpreetFrameAgency.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
